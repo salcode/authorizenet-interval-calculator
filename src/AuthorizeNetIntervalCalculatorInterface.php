614718a8256b88f0e6b058be5438544f79d25fef
Sal Ferrarello <sal@ironcodestudio.com>
2019-02-20 15:42:44 -0500

    Add AuthorizeNetIntervalCalculatorInterface

---
+++ src/AuthorizeNetIntervalCalculatorInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * AuthorizeNetIntervalCalculator
+ *
+ * @package   salcode\AuthorizeNetIntervalCalculator
+ * @author    Sal Ferrarello <sal@ironcodestudio.com>
+ * @copyright 2018 Sal Ferrarello, Iron Code Studio
+ * @license   apache-2.0
+ */
+
+declare(strict_types=1);
+
+namespace salcode\AuthorizeNetIntervalCalculator;
+
+/**
+ * Interface: AuthorizeNetIntervalCalculatorInterface
+ */
+interface AuthorizeNetIntervalCalculatorInterface
+{
+}

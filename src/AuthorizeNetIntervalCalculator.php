63dbbd3d29e03afecfd7aa17c62ae6d1ccdbc7f7
Sal Ferrarello <sal@ironcodestudio.com>
2019-02-22 15:50:51 -0500

    Comment tidying

--- src/AuthorizeNetIntervalCalculator.php
+++ src/AuthorizeNetIntervalCalculator.php
@@ -60,17 +60,17 @@ class AuthorizeNetIntervalCalculator implements AuthorizeNetIntervalCalculatorIn
      * Constructor
      *
      * @param int               $length The measurement of time, in association
-     *                  with unit, that is used to define the frequency of the
-     *                  billing occurrences. For a unit of days, use an integer
-     *                  between 7 and 365, inclusive. For a unit of months,
-     *                  use an integer between 1 and 12, inclusive.
+     *        with unit, that is used to define the frequency of the billing
+     *        occurrences. For a unit of days, use an integer between 7 and
+     *        365, inclusive. For a unit of months, use an integer between 1
+     *        and 12, inclusive.
      * @param string            $unit The unit of time, in association with the
-     *                  length, between each billing occurrence. Either 'days'
-     *                  or 'months'.
+     *        length, between each billing occurrence. Either 'days' or
+     *        'months'.
      * @param DateTimeInterface $startDate The date of the first payment.
      *
      * @throws InvalidArgumentException When length and unit are not a
-     *                                  valid combination.
+     *         valid combination.
      */
     public function __construct(int $length, string $unit, DateTimeInterface $startDate)
     {
@@ -87,7 +87,7 @@ class AuthorizeNetIntervalCalculator implements AuthorizeNetIntervalCalculatorIn
      *
      * @param int    $length The number of units in an interval.
      * @param string $unit   The unit to be used with the length when
-     *                       determining the interval.
+     *        determining the interval.
      *
      * @return bool The combination of length and unit are valid.
      */
